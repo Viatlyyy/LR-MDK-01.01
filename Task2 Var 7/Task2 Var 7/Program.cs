3d7d589f41ffbb7e70d39b5069edc127272131c8
Krilo <krilow.vitaly007@gmail.com>
2025-09-25 15:41:04 +0300

    Создаем генератор случайных числел, создаем переменную с размером массива, и создаем сам массив случайных чисел

---
+++ Task2 Var 7/Task2 Var 7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_Var_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int array = 2500;
+
+            int[] numbers = new int[array];
+            for (int i = 0; i < array; i++)
+            {
+                numbers[i] = random.Next(-2000, 2001);
+            }
+
+
+        }
+    }
+}
