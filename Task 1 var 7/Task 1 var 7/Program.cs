71e5680faf80f44b43875bf96828e1a8026bf169
Krilo <krilow.vitaly007@gmail.com>
2025-09-25 15:19:46 +0300

    Получаем количество элементов от пользователя и создаем массив нужного размера

---
+++ Task 1 var 7/Task 1 var 7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_var_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int n;
+          Console.Write("Введите количество чисел в массиве: ");
+          n = Convert.ToInt32(Console.ReadLine());
+
+          int[] numbers = new int[n];
+
+        }
+    }
+}
