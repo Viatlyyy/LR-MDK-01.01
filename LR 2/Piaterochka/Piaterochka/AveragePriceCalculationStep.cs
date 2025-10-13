a439ef1f85f44b574629a471a61c8ec25a815a15
Krilo <krilow.vitaly007@gmail.com>
2025-10-13 20:01:09 +0300

    Создание класса AveragePriceCalculationStep.cs для подсчета средней цены продуктов из определенной категории

---
+++ LR 2/Piaterochka/Piaterochka/AveragePriceCalculationStep.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piaterochka
+{
+    class AveragePriceCalculationStep
+    {
+        public decimal CalculateAveragePrice(List<decimal> prices)
+        {
+            if (prices == null || prices.Count == 0)
+            {
+                return 0;
+            }
+
+            decimal sum = 0;
+            foreach (decimal price in prices)
+            {
+                sum += price;
+            }
+
+            return sum / prices.Count;
+        }
+    }
+}
