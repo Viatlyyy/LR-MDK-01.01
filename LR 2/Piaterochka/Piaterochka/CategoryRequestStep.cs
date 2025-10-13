6f6ec655a8639825168a84a0f5ec41cfc978157b
Krilo <krilow.vitaly007@gmail.com>
2025-10-13 19:51:54 +0300

    Создание класса, позволяющего пользователю выбрать определенную категорию продуктов

---
+++ LR 2/Piaterochka/Piaterochka/CategoryRequestStep.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piaterochka
+{
+    class CategoryRequestStep
+    {
+        public string RequestCategory()
+        {
+            Console.WriteLine("Выберите категорию продуктов (Крупы, Фрукты, Овощи):");
+            return Console.ReadLine();
+        }
+    }
+}
