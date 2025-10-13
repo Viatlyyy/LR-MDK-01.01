9704c29b98a662dd2c7723c4e5078fda0ec3900d
Krilo <krilow.vitaly007@gmail.com>
2025-10-13 19:58:12 +0300

    Создание класса Product.cs с созданием переменных имени продукта, его категории и списка с историей цен

---
+++ LR 2/Piaterochka/Piaterochka/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piaterochka
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public List<decimal> PriceHistory { get; set; }
+    }
+}
