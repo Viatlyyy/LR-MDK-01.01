05764c0d8bd4d4e76975d2187a4d280d649c4f2d
Krilo <krilow.vitaly007@gmail.com>
2025-10-13 20:18:58 +0300

    Полная доработка класса Program с вызовом всех классов и функций, а также добавление вывода товаров из категорий для пользователя

--- LR 2/Piaterochka/Piaterochka/Program.cs
+++ LR 2/Piaterochka/Piaterochka/Program.cs
@@ -6,11 +6,37 @@ using System.Threading.Tasks;
 
 namespace Piaterochka
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
 
+            DataFetchingStep dataFetcher = new DataFetchingStep();
+            List<Product> products = dataFetcher.GetProductData();
+
+
+            CategoryRequestStep categoryRequester = new CategoryRequestStep();
+            string userCategory = categoryRequester.RequestCategory();
+
+
+            ProductSearchStep productSearcher = new ProductSearchStep();
+            List<Product> productsInCategory = productSearcher.FindProductsByCategory(userCategory, products);
+
+
+            AveragePriceCalculationStep priceCalculator = new AveragePriceCalculationStep();
+            ProductSortingStep productSorter = new ProductSortingStep();
+
+            List<Product> sortedProducts = productSorter.SortProducts(productsInCategory);
+
+
+            Console.WriteLine("Товары в категории:");
+            foreach (Product product in sortedProducts)
+            {
+                decimal averagePrice = priceCalculator.CalculateAveragePrice(product.PriceHistory);
+                Console.WriteLine($"- {product.Name} - {averagePrice} rub.");
+            }
+
+            Console.ReadKey();
         }
     }
 }
