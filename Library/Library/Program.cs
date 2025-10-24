f9c022f610d99e703384d2fb6dca9c64f0d0a9d7
Krilo <krilow.vitaly007@gmail.com>
2025-10-24 20:48:53 +0300

    Создание класса SimpleLibrary с двумя списками для хранения названия книги и для хранения ее номера, также была добавлена переменная для присвоения следующего номера для книги

---
+++ Library/Library/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SimpleLibrary
+{
+
+    List<string> bookTitles = new List<string>();
+    List<int> bookNumbers = new List<int>();
+
+
+    int nextNumber = 1;
+}
