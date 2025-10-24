589f1833b224dbaaca262f73e1e37063e854825f
Krilo <krilow.vitaly007@gmail.com>
2025-10-24 20:54:15 +0300

    Добавление фнункции Run c выводом пользователю меню с выбором функций и добавление switch, который решает какое действие выполнить на основе выбора пользователя.

--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -12,4 +12,41 @@ public class SimpleLibrary
 
 
     int nextNumber = 1;
+    public void Run()
+    {
+        bool keepRunning = true;
+        Console.WriteLine("--- Простейший Менеджер Книг ---");
+
+        while (keepRunning)
+        {
+            Console.WriteLine("Меню:");
+            Console.WriteLine("1. Добавить книгу");
+            Console.WriteLine("2. Показать книги");
+            Console.WriteLine("3. Изменить название книги");
+            Console.WriteLine("4. Выход");
+            Console.Write("Выберите действие (введите число): ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    AddBook();
+                    break;
+                case "2":
+                    ShowBooks();
+                    break;
+                case "3":
+                    EditBook();
+                    break;
+                case "4":
+                    keepRunning = false;
+                    Console.WriteLine("До свидания!");
+                    break;
+                default:
+                    Console.WriteLine("Неправильный ввод. Попробуйте ещё раз.");
+                    break;
+            }
+        }
+    }
 }
