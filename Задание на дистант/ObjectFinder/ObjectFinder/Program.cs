ef738bd935b36235fe14e027fc71f2e31f399fa5
Виталий <krilow.vitaly007@gmail.com>
2025-11-12 20:49:04 +0300

    Реализация Program с вызовом всех классов и функций для генерации фруктов, поиска лучших 10 и вывода их на консоль.

--- Задание на дистант/ObjectFinder/ObjectFinder/Program.cs
+++ Задание на дистант/ObjectFinder/ObjectFinder/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace ObjectFinder
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Fruit[] allFruits = FruitGenerator.Create100RandomFruits();
+
+            FruitFinder.CharacteristicInfo[] top10 = FruitFinder.FindTop10MaxCharacteristics(allFruits);
+
+            Console.WriteLine("ТОП 10 фруктов по наибольшим значениям характеристик");
+            if (top10.Length == 0)
+            {
+                Console.WriteLine("Не найдено фруктов.");
+            }
+            else
+            {
+                for (int i = 0; i < top10.Length; i++)
+                {
+                    FruitFinder.CharacteristicInfo info = top10[i];
+                    Console.WriteLine($"{info.fruit.ToString()} (по характеристике '{info.characteristicName}' со значением {info.value:F1})");
+                }
+            }
         }
     }
 }
