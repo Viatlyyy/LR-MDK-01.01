4301a37277b9e8d0c1ef0aeb45c2f684286e3f27
Виталий <krilow.vitaly007@gmail.com>
2025-11-17 19:33:08 +0300

    Создание класса SalesData,отвечающий за хранение и управление коллекцией всех записей о продажах.

---
+++ PhoneSalesAnalyzer/PhoneSalesAnalyzer/PhoneSalesAnalyzer/SalesData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SalesData
+{
+    private List<SaleRecord> salesList;
+
+    public SalesData()
+    {
+        salesList = new List<SaleRecord>();
+    }
+
+    public void AddSale(SaleRecord sale)
+    {
+        if (sale.PhoneModel != null)
+        {
+            salesList.Add(sale);
+        }
+    }
+
+    public IEnumerable<SaleRecord> GetAllSales()
+    {
+        return salesList;
+    }
+
+    public void ClearSales()
+    {
+        salesList.Clear();
+    }
+}
