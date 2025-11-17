ba1e108f075a0c4f975d98c2d2be6ea5a60139cb
Виталий <krilow.vitaly007@gmail.com>
2025-11-17 19:30:56 +0300

    Создание структуры SaleRecord, представляющая одну запись о продаже мобильного телефона.

---
+++ PhoneSalesAnalyzer/PhoneSalesAnalyzer/PhoneSalesAnalyzer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PhoneSalesAnalyzer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PhoneSalesAnalyzer")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2ee1d569-2833-4863-a9e7-0f1c4b6280e8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PhoneSalesAnalyzer/PhoneSalesAnalyzer/PhoneSalesAnalyzer/SaleRecord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public struct SaleRecord
+{
+    public DateTime SaleDate;
+    public string PhoneModel;
+    public int QuantitySold;
+    public double PricePerUnit;
+    public double CostPerUnit;
+
+    public SaleRecord(DateTime saleDate, string phoneModel, int quantitySold, double pricePerUnit, double costPerUnit)
+    {
+        SaleDate = saleDate;
+        PhoneModel = phoneModel;
+        QuantitySold = quantitySold;
+        PricePerUnit = pricePerUnit;
+        CostPerUnit = costPerUnit;
+    }
+
+    public double CalculateProfit()
+    {
+        if (CostPerUnit > PricePerUnit)
+        {
+            return 0;
+        }
+        return (PricePerUnit - CostPerUnit) * QuantitySold;
+    }
+}
+
