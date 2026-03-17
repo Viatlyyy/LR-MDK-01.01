970ddd857f5b2f7e04ec148a0194a2bdb660ad9b
Irina1915 <iryna.budina@yandex.ru>
2026-03-17 11:13:41 +0300

    BD

---
+++ BD/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace BD
+{
+    public partial class MainForm: Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            var cs = "Host=192.168.1.48;Username=st50-4;Password=504;Database=proptest";
+
+            var con = new NpgsqlConnection(cs);
+            con.Open();
+
+
+            string sql = "SELECT login, password FROM myusers";
+            var cmd = new NpgsqlCommand(sql, con);
+
+            var reader = cmd.ExecuteReader();
+            List<string> allLogin = new List<string>();
+            while (reader.Read())
+            {
+                allLogin.Add(reader.GetString(0) + ":" + reader.GetString(1));
+
+            
+            }
+            
+            MessageBox.Show($"Login: {string.Join(", ", allLogin)}");
+            
+            
+            con.Close();
+        }
+    }
+}
---
+++ BD/PostgresUsersLoader.cs
@@ -0,0 +1,45 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BD
+{
+    public class PostgresUsersLoader
+    {
+        public List<User> Load()
+        {
+            
+            var cs = "Host=192.168.1.48;Username=st50-4;Password=504;Database=proptest";
+
+            var con = new NpgsqlConnection(cs);
+            con.Open();
+
+
+            string sql = "SELECT login, password FROM myusers";
+            var cmd = new NpgsqlCommand(sql, con);
+
+            var reader = cmd.ExecuteReader();
+            List<User> allUsers = new List<User>();
+            while (reader.Read())
+            {
+                allUsers.Add(reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
+
+            }
+
+            MessageBox.Show($"Login: {string.Join(", ", allUsers)}");
+
+
+            con.Close();
+
+            return allUsers;
+        }
+    }
+}
+
+        
+    
+
